e3d6c792882b0347ab93cfa0df2c24187e67c760
Pidassaa <pidassaa.nwebida@centrale-casablanca.ma>
2022-11-02 16:20:33 +0100

    Premier jet classe mère Vaisseau

---
+++ Vaisseau.py
@@ -0,0 +1,15 @@
+class Vessel:
+
+    def __init__(self, coordinates, max_hits, weapon):
+
+        self.coordinates = coordinates
+        self.max_hits = max_hits
+        self.weapon = weapon
+
+    def go_to(self, x, y, z):
+
+    def get_coordinates(self):
+
+    def fire_at(self, x, y, z):
+
+    pass
