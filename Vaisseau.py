ce265556f9e9cb66a57e34026e6b769bc167497f
Pidassaa <pidassaa.nwebida@centrale-casablanca.ma>
2022-11-02 18:18:19 +0100

    Second jet classe mère Vaisseau

--- Vaisseau.py
+++ Vaisseau.py
@@ -7,9 +7,19 @@ class Vessel:
         self.weapon = weapon
 
     def go_to(self, x, y, z):
+        try:
+            self.coordinates = (x, y, z)
+            assert self.max_hits > 0
+        except AssertionError:
+            print(" le navire est détruis, il ne peut donc se deplacer ")
 
     def get_coordinates(self):
 
-    def fire_at(self, x, y, z):
+        print("les coordonnées actuelles du navire sont : {0}".format(self.coordinates))
 
-    pass
+    def fire_at(self, x, y, z):
+        try:
+            self.weapon.fire_at(x, y, z)  # on utilise la fonction fire_at de la classe weapon déjà définie
+            assert self.max_hits > 0
+        except AssertionError:
+            print(" le navire est détruis, il ne peut donc tirer ")
