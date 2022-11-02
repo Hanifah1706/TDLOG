9e3ca9a30fbda9db1725f439c564963bc2ca8cca
cheikhabdoul <114985963+cheikhabdoul@users.noreply.github.com>
2022-11-02 21:11:17 +0100

    Update test_fact.py

--- test_fact.py
+++ test_fact.py
@@ -5,7 +5,6 @@ import Factoriel
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(math.factorial(5), Factoriel.fact(5))  # add assertion here
- "écris pas cheick"
 
 
 
