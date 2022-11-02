3ae4239382b7d2ae7c41db1ae8d803ed89df957c
Pidassaa <pidassaa.nwebida@centrale-casablanca.ma>
2022-11-02 23:59:31 +0100

    Supression des anciennes modifications(commentaires)

--- Factoriel.py
+++ Factoriel.py
@@ -1,8 +1,5 @@
 def fact(n):
-    if n == 0 or n==1:
-        #commentaires
-        #Fatime
-        #commentaires Modibo
+    if n == 0 or n==1:#les dernières modification était des tests; ceci également
         return 1
     return n*fact(n-1)
 
