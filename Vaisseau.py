c8150ad67a6747146dcbd305dce4bdc744e89c8b
Pidassaa <pidassaa.nwebida@centrale-casablanca.ma>
2022-11-03 13:06:12 +0100

    Import class weapon

--- Vaisseau.py
+++ Vaisseau.py
@@ -1,6 +1,7 @@
+from Weapon import *
 class Vessel:# classe mère
 
-    def __init__(self, coordinates, max_hits, weapon):
+    def __init__(self, coordinates: tuple, max_hits: int, weapon: Weapon):
 
         self.coordinates = coordinates
         self.max_hits = max_hits
@@ -9,8 +10,9 @@ class Vessel:# classe mère
     def go_to(self, x, y, z):
         try:
             self.coordinates = (x, y, z)
-            assert self.max_hits > 0
-        except AssertionError:
+            if self.max_hits <= 0:
+                raise DestroyedError(le destroyer détruis)
+        except DestroyedError:
             print(" le navire est détruis, il ne peut donc se deplacer ")
 
     def get_coordinates(self):
@@ -20,6 +22,6 @@ class Vessel:# classe mère
     def fire_at(self, x, y, z):
         try:
             self.weapon.fire_at(x, y, z)  # on utilise la fonction fire_at de la classe weapon déjà définie
-            assert self.max_hits > 0
-        except AssertionError:
+            raise DestroyedError(le destroyer détruis)
+        except DestroyedError:
             print(" le navire est détruis, il ne peut donc tirer ")
