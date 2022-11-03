1c6b24d5c9089144c1702c73021be55d9f66d8d9
NabibaGBANE <114986547+NabibaGBANE@users.noreply.github.com>
2022-11-03 12:43:39 +0100

    Delete weapon_lm.py

--- weapon_lm.py
+++
@@ -1,9 +0,0 @@
-class lances_torpilles(Weapon):
-    def __init__(self,ammunitions,range):
-         self.range = 20
-         self.ammunitions = 15
-    def fire_at(self,x,y,z):
-        if self.ammunitions==0:
-            raise NoAmminitionError
-        if self.z <=0:
-            raise OutOfRangeError
