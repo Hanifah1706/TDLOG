04a574c2cac3b0476696b77467e5406e4a2ace13
Hanifah <hanifahsaw@gmail.com>
2022-11-05 00:40:34 +0100

    vaisseau Destroyer ajouté

--- Destroyer.py
+++ Destroyer.py
@@ -1,9 +1,10 @@
 from Vaisseau import Vessel
+import Lance_missiles_anti_air
 #import Lance_Torpille as lt
 class Destroyer(Vessel):
     def __init__(self, coordinates):
-        Vessel.__init__(self, coordinates)
-        self.max_hits=4
+        Vessel.__init__(coordinates, max_hits=4,weapon=Lance_missiles_anti_air())
+        if self.coordinates
         #self.weapon=lt.Lance_torpilles
 
     def go_to(self,x,y,z):
@@ -12,6 +13,8 @@ class Destroyer(Vessel):
             assert z==0
         except AssertionError:
             print("le deplacement n'est possible que pour z=0")
+Bateau= Destroyer((3,4,5))
+Bateau.go_to((3,4,5))
 
 
 
