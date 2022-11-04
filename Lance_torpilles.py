dc86cc3ac125aa8860dee5db770f6df30b034787
NabibaGBANE <114986547+NabibaGBANE@users.noreply.github.com>
2022-11-04 15:37:30 +0100

    Add files via upload

--- Lance_torpilles.py
+++ Lance_torpilles.py
@@ -7,11 +7,11 @@ class lances_torpilles(Weapon):
          self.range = 20
          self.ammunitions = 15
     def fire_at(self, x:int, y:int, z:int):
-        super().fire_at()
+        super().fire_at(x, y, z)
         if self.z <=0:
            print("cible atteignable")
         else:
             raise OutOfRangeError("cible inatteignable")
-            self.fire_at(x, y, z)
+
 
 
