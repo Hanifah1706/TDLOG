679aad80a0d036d8fdcf18850a8bdc7694e8678f
Pidassaa <114985313+Pidassaa@users.noreply.github.com>
2022-11-04 18:28:34 +0100

    Update Vaisseau.py

--- Vaisseau.py
+++ Vaisseau.py
@@ -13,11 +13,12 @@ class Vessel:# classe mère
 
     def go_to(self, x, y, z):
         try:
-            self.coordinates = (x, y, z)
             if x > 100 or y > 100 or x < 0 or y < 0 or z not in [1, 0, -1]:
-                raise OutOfRangeError
-            if self.max_hits <= 0:
+                raise OutOfRangeError("Impossible de ce déplacer à cette position")
+            elif self.max_hits <= 0:
                 raise DestroyedError("le destroyer détruis")
+            else:
+                self.coordinates = (x, y, z)
         except DestroyedError:
             print(" le navire est détruis, il ne peut donc se deplacer ")
         except OutOfRangeError:
@@ -28,17 +29,18 @@ class Vessel:# classe mère
 
     def fire_at(self, x, y, z):
         try:
-            self.weapon.fire_at(x, y, z)# on utilise la fonction fire_at de la classe weapon déjà définie
             if self.max_hits <= 0:
                 raise DestroyedError("le destroyer détruis")
-            if distance(self.coordinates,(x,y,z))>self.weapon.range:
-                raise OutOfRangeError
+            elif distance(self.coordinates,(x,y,z))>self.weapon.range:
+                raise OutOfRangeError("Distance hors d'atteinte!")
+            else:
+                self.weapon.fire_at(x, y, z)  # on utilise la fonction fire_at de la classe weapon déjà définie
         except DestroyedError:
             print(" le navire est détruis, il ne peut donc tirer ")
         except OutOfRangeError:
-            print("La cible est hors d'atteinte")
+            print("La cible est hors d'atteinte! Veuillez vous rapprocher")
             self.weapon.fire_at(x, y, z)
 
 def distance(A : tuple , B : tuple):
     d=sqrt((A[0]-B[0])**2 + (A[1]-B[1])**2 + (A[2]-B[2])**2)
-    return d
+    return d  #retour de la distance entre deux points
