53037a2b2de7abcd84fca66b2f36af6d4f739f59
NabibaGBANE <114986547+NabibaGBANE@users.noreply.github.com>
2022-11-04 17:28:37 +0100

    Add files via upload

--- Lance_torpilles.py
+++ Lance_torpilles.py
@@ -1,12 +1,13 @@
 from Exception import OutOfRangeError
 from Weapon import Weapon
-
+import unittest
 class lances_torpilles(Weapon):
-    def __init__(self,):
-         super().__init__()
-         self.range = 20
-         self.ammunitions = 15
+    def __init__(self):
+         super().__init__(ammunitions=15,range=20)
+         #self.range = 20
+         #self.ammunitions = 15
     def fire_at(self, x:int, y:int, z:int):
+        self.z=z
         super().fire_at(x, y, z)
         if self.z <=0:
            print("cible atteignable")
@@ -15,3 +16,5 @@ class lances_torpilles(Weapon):
 
 
 
+A=lances_torpilles()
+A.fire_at(1,1,-1)
