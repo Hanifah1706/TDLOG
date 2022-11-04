aa07aa560738c408e399ba975c1a5cff13f9e2bf
fatyme <114987882+fatyme@users.noreply.github.com>
2022-11-04 16:11:53 +0100

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -1,15 +1,16 @@
 from Exception import NoAmmunitionError
 
+
 class Weapon:
     def __init__(self, ammunitions: abs, range: int):
-        
+
         self.ammunitions = ammunitions
         self.range = range
 
     def fire_at(self, x: int, y: int, z: int):
         if not self.ammunitions != 0:
-            raise NoAmmunitionError(ammunitions)
-        if self.ammunitions != 0 :
+            raise NoAmmunitionError(self.ammunitions)
+        if self.ammunitions != 0:
             self.ammunitions = self.ammunitions - 1
 
 
