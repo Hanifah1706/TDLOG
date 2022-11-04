343c21c104204e2605fcace4a7292a40175c772b
Hanifah <hanifahsaw@gmail.com>
2022-11-04 17:19:52 +0100

    verifié

--- Lance_missiles_anti_air.py
+++ Lance_missiles_anti_air.py
@@ -2,15 +2,15 @@ from Weapon import Weapon
 from Exception import OutOfRangeError
 class Lance_missiles_anti_air(Weapon):
     def __init__(self):
-        #super().__init__(self, ammunitions, range)
+        super().__init__( ammunitions=50, range=40)
 
-        self.range = 40
-        self.ammunitions = 50
 
     def fire_at(self, x: int, y: int, z: int):
-        if z<= 0 :
-            super().fire_at(self,x,y,z)
-            raise OutOfRangeError
-            print("hors de la zone de tir!")
+        self.z=z
+        if self.z<0 or self.z== 0 :
+            super().fire_at(x,y,z)
+            raise OutOfRangeError ("hors de la zone de tir!")
         else:
             print("tir autorisé ")
+arm=Lance_missiles_anti_air()
+arm.fire_at(2,10,0)
