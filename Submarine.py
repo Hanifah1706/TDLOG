bb9ec1dd645ab6608ddedab3e4c7c1376d44cf6d
Modibo TRAORE <modibo.traore@centrale-casablanca.ma>
2022-11-04 14:32:45 +0100

    test

--- Submarine.py
+++ Submarine.py
@@ -1,16 +1,14 @@
 from Vaisseau import Vessel
-from Weapon import Weapon
 #from arme import lance_tropille
 
-class Submarine(Vessel, Weapon):
-    def __init__(self, coordinates, max_hits, weapon,x, y,z, ammunitions, range):
+class Submarine(Vessel):
+    def __init__(self, coordinates, max_hits, weapon):
         Vessel.__init__(coordinates, max_hits, weapon)
-        Weapon.__init__(x, y, z, ammunitions, range)
         self.max_hits = 2
         self.ammunitions =15
         self.range = 20
 
-        #self.armement = lance_tropille
+       
 
     def go_to(self, x, y, z):
         if self.coordinates == (x, y, -1) or self.coordinates == (x, y, 0):
