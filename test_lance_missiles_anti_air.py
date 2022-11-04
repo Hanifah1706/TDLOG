c28e4c2c15b4204c0cc5608feff3a09ce61638a2
Hanifah <hanifahsaw@gmail.com>
2022-11-05 00:25:12 +0100

    test

--- test_lance_missiles_anti_air.py
+++ test_lance_missiles_anti_air.py
@@ -5,8 +5,8 @@ from Exception import *
 arm=Lance_missiles_anti_air()
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertRaises(OutOfRangeError, arm.fire_at(0,5,-7))  # add assertion here
+        self.assertRaises(OutOfRangeError, arm.fire_at(0, 5, 2))  # add assertion here
 
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+ #   unittest.main()
