610901a176992e0ebc0ce1ee051354fbf099de78
Pidassaa <pidassaa.nwebida@centrale-casablanca.ma>
2022-11-04 18:12:19 +0100

    Mis à jour de Espace et Vaisseau

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from Vaisseau import *
 from Weapon import *
 
+"""
 Arme=Weapon(50,50)
 Bateau=Vessel((1, 1, 1), 6, Arme)
 Bateau.fire_at(100,1,1)
+Arme=Weapon(0,50)
+Bateau1=Vessel((1, 1, 1), 6, Arme)
+Bateau1.fire_at(10,1,1)"""
+Arme=Weapon(50,50)
+A=Vessel((1,1,1), "a", 10)
+print(A.coordinates)
+A.go_to(1,2,2)
+print(A.coordinates)
+
