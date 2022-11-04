0ad8b9ed2a2d5b6425a78711bcde8756c2ac4a32
Modibo TRAORE <modibo.traore@centrale-casablanca.ma>
2022-11-04 18:19:21 +0100

    Classe Submarine

--- Submarine.py
+++ Submarine.py
@@ -1,33 +1,36 @@
 from Vaisseau import Vessel
-
+from Exception import OutOfRangeError
+from Lance_torpilles import Lances_torpilles
 
 class Submarine(Vessel):
-    def __init__(self, coordinates, max_hits, weapon):
-        Vessel.__init__(coordinates, max_hits, weapon)
-        self.max_hits = 2
-        self.ammunitions =15
-        self.range = 20
+    def __init__(self, coordinates):
+        Vessel.__init__(self, coordinates, max_hits= 2, weapon=Lances_torpilles())
+        if self.coordinates[2] not in [-1,0]:
+            raise OutOfRangeError("Ne prend que les coordonnées z=0 ou -1")
 
 
 
-    def go_to(self, x, y, z):
-        if self.coordinates == (x, y, -1) or self.coordinates == (x, y, 0):
-            self.go_to(x,y,z)
-            print( " Le vaisseau se deplace")
+
+    def go_to(self, x, y,z):
+
+        if z == -1 or z == 0:
+            super().go_to(x, y, z)
+            print(" Le vaisseau se deplace")
         else:
-            raise OutOfRangeError
+            raise OutOfRangeError("Pas de déplacement du vaisseau")
             print("Le deplacement n'est pas possible")
 
 
 
-   # def fire_at(self, x, y, z):
-    #    self.weapon.fire_at(x, y, z)
-    #   if self.max_hits == 0:
-    #        print( " DestroyedError")
 
-    #    while d > self.range :
-    #        print("OutOfRangeError")
-    #        self.ammunitions -= 1
+
+A= Submarine((1, 2, 0))
+print(A.coordinates)
+A.go_to(2,2,-1)
+print(A.coordinates)
+print(A.max_hits)
+
+
 
 
 
