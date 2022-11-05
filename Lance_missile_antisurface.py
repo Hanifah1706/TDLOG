708a8002bbe42bbbdb91f6893989cd79cd439947
Pidassaa <114985313+Pidassaa@users.noreply.github.com>
2022-11-05 10:47:22 +0100

    Update Lance_missile_antisurface.py

--- Lance_missile_antisurface.py
+++ Lance_missile_antisurface.py
@@ -11,7 +11,7 @@ class Lance_missile_antisurface(Weapon):
 
     def fire_at(self, x: int, y: int, z: int):
         if self.z != 0:
-            super().fire_at(x, y, z)
+            super().fire_at(x, y, z)# On fait appel à cette méthode pour dimunier les ammunitions
             raise OutOfRangeError("la cible n'est pas admissible")
         else:
             print("la cible est atteignable")
