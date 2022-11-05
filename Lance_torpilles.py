752fd6f81f2c41e981c7d9436b09bd111f820606
Pidassaa <114985313+Pidassaa@users.noreply.github.com>
2022-11-05 11:17:05 +0100

    Update Lance_torpilles.py

--- Lance_torpilles.py
+++ Lance_torpilles.py
@@ -8,13 +8,11 @@ class Lances_torpilles(Weapon):
          #self.ammunitions = 15
     def fire_at(self, x:int, y:int, z:int):
         self.z=z
-        super().fire_at(x, y, z)
         if self.z <=0:
            print("cible atteignable")
         else:
+            super().fire_at(x, y, z)# On fait appel à cette méthode pour dimunier les ammunitions
             raise OutOfRangeError("cible inatteignable")
 
 
 
-A=Lances_torpilles()
-A.fire_at(1,1,-1)
